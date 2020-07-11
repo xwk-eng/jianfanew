cc7100731006b62a870f6dcb6a9ebafdee71a2fc
xwk-eng <1411681860@qq.com>
2020-07-11 09:07:54 +0800

    jiangfanew

---
+++ src/main/java/com/atguigu/huashan/jianfanew2.java
@@ -0,0 +1,19 @@
+package com.atguigu.huashan;
+
+/**
+ * @author xwkstart
+ * @create 2020-07-11 9:06
+ */
+public class jianfanew2 {
+
+    public static void main(String[] args) {
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+        System.out.println("dugu9999999999999jian");
+
+
+
+    }
+
+}
